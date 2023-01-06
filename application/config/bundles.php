4e2ec5ac02e56d750e795b77fb2f139033a361d1
Youri Lefers <youri.lefers@x-ip.nl>
2023-01-06 21:18:12 +0000

    Added image pushing

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -5,6 +5,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- application/deploy.Dockerfile
+++ application/deploy.Dockerfile
@@ -1,8 +1,9 @@
 FROM php
 
-COPY --from=ghcr.io/xip-online-applications/php-docker-containers/php-extra-prod /opt /opt
+COPY --from=ghcr.io/xip-online-applications/php-docker-containers/php-extra-prod:8.1 /opt /opt
 
 ARG COMPOSER_AUTH=""
 RUN bash -c "COMPOSER_AUTH='$COMPOSER_AUTH' composer install --no-dev --optimize-autoloader --no-interaction --no-scripts"
-RUN composer dump-env prod \
-    && bin/console cache:warmup --env=prod
+
+RUN composer dump-env prod
+RUN bin/console cache:warmup --env=prod
